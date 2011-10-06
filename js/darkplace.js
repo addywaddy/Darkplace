8621fd347b10dec9e37f88f9ae9e5736f8fb6a8e
Adam Groves <adam.groves@gmail.com>
2011-10-06 21:10:32 +0200

    tidied up localStorage stuff

--- js/darkplace.js
+++ js/darkplace.js
@@ -1,68 +1,102 @@
-Darkplace = {
-  open: function(name) {
-    localStorage.setItem("_DP", name)
-    textArea.value = localStorage.getItem("DP:" + localStorage.getItem("_DP"))
+DP = {
+  store: {
+    all: function() {
+      return Object.keys(localStorage).filter(function(key) {
+        return key.match(/^DP:/)
+      })
+    },
+
+    getCurrent: function() {
+      return localStorage.getItem('_DP')
+    },
+
+    setCurrent: function(val) {
+      localStorage.setItem("_DP", val)
+      return this.getCurrent()
+    },
+
+    get: function(name) {
+      return localStorage.getItem("DP:" + name)
+    },
+
+    set: function(name, val) {
+      return localStorage.setItem("DP:" + name, val)
+    },
+
+    delete: function(name) {
+      localStorage.removeItem("DP:" + name)
+    }
   },
+  open: function(name) {
+    textArea.value = DP.store.get( DP.store.setCurrent(name) )
+  },
+
   openOrDelete: function(e) {
     if (e.target.className == 'destroy') {
       var name = e.target.parentElement.firstChild.textContent
       if (confirm("Sure you want to delete '" + name + "'?")) {
-        if (localStorage.getItem('_DP') == name) {
-          localStorage.setItem("_DP")
+        if (DP.store.getCurrent() == name) {
+          // Clears the current page
+          DP.store.setCurrent('')
         }
-        localStorage.removeItem("DP:" + name)
-        Darkplace.renderMenu()
+        DP.store.delete(name)
+        DP.renderMenu()
       }
     } else {
       var name = e.target.firstChild.textContent
-      Darkplace.open(name)
+      DP.open(name)
     }
     return false;
   },
 
   save: function() {
-    if(localStorage.getItem("_DP")) {
+    if(DP.store.getCurrent()) {
       console.log("saving")
-      localStorage.setItem("DP:" + localStorage.getItem("_DP"), textArea.value)
+      DP.store.set(DP.store.getCurrent(), textArea.value)
     }
   },
 
   renderMenu: function() {
    document.querySelector("#darkplaces").innerHTML = ""
    var list = [], link, li
-   Object.keys(localStorage).forEach(function(key) {
-     if (key.match(/^DP:/)) {
-       with (link = document.createElement('a')) {
-         setAttribute('href', '')
-         innerHTML = "X"
-         setAttribute("class", 'destroy')
-       }
-
-       with ((li = document.createElement('li'))) {
-         appendChild(document.createTextNode(key.match(/^DP:(.+)$/)[1]))
-         appendChild(link)
-       }
-       document.querySelector("#darkplaces").appendChild(li)
+   DP.store.all().forEach(function(key) {
+     with (link = document.createElement('a')) {
+       setAttribute('href', '')
+       innerHTML = "x"
+       setAttribute("class", 'destroy')
      }
+
+     with ((li = document.createElement('li'))) {
+       appendChild(document.createTextNode(key.match(/^DP:(.+)$/)[1]))
+       appendChild(link)
+     }
+     document.querySelector("#darkplaces").appendChild(li)
    })
   },
+
   create: function(e) {
     setTimeout(function() {
       if (e.keyCode == 13 && !e.target.value.match(/^\s+$/)) {
-        console.log("saving")
-        localStorage.setItem("DP:" + e.target.value, "");
-        localStorage.setItem("_DP", e.target.value)
+        window.clearInterval(interval)
+        DP.store.set(e.target.value, '')
+        DP.store.setCurrent(e.target.value)
         e.target.value = ''
-        Darkplace.renderMenu()
+        DP.open(DP.store.getCurrent())
+        interval = setInterval(DP.save, 2e3)
+        DP.renderMenu()
       }
     }, 0);
+  },
+
+  start: function() {
+    DP.renderMenu()
+    DP.open(DP.store.getCurrent())
   }
 }
 
-document.querySelector("#newDarkplace").addEventListener("keypress", Darkplace.create, false)
-document.querySelector("#darkplaces").addEventListener("click", Darkplace.openOrDelete, true)
+document.querySelector("#newDarkplace").addEventListener("keypress", DP.create, false)
+document.querySelector("#darkplaces").addEventListener("click", DP.openOrDelete, false)
 var textArea = document.querySelector("#darkplace")
 
-Darkplace.renderMenu()
-Darkplace.open(localStorage.getItem('_DP'))
-var interval = setInterval(Darkplace.save, 2e3)
+var interval = setInterval(DP.save, 2e3)
+DP.start()
